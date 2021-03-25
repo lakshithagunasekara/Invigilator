18b66a929b6130c3be72a165334ef67608010070
Shan Wijenayaka <shanwijenayaka@outlook.com>
2021-03-25 16:13:16 +0530

    removed keys from repo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,22 +7,6 @@ import * as AWS from "aws-sdk";
 
 function App() {
 
-    // const SESConfig = {
-    //     apiVersion: "2010-12-01",
-    //     accessKeyId: "AKIAXJTZRA37HBWZUM4U",
-    //     accessSecretKey: "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",
-    //     region: "us-east-1"
-    // }
-    // // AWS.config.update(SESConfig);
-    //
-    // AWS.config.update(SESConfig);
-    // AWS.config.update({
-    //     credentials: new AWS.CognitoIdentityCredentials({
-    //         IdentityPoolId: 'us-east-1:f1899ed5-00c6-4808-943a-ba0ef645a8f7',
-    //     }),
-    //     region: 'us-west-2'
-    // });
-
 // Initialize the Amazon Cognito credentials provider
     AWS.config.region = 'us-east-1'; // Region
     AWS.config.credentials = new AWS.CognitoIdentityCredentials({
@@ -34,17 +18,6 @@ function App() {
             Bucket: 'shanwije-inv-0419'
         }
     });
-    // REACT_APP_ACCESS_ID=AKIAXJTZRA37HBWZUM4U
-    // REACT_APP_ACCESS_KEY=RdMu2JiT0mHvJTKzKg7Zmp4rP9RTi2r2RfTpjnWnXXXXXX
-    // REACT_APP_BUCKET_NAME=invtest2
-    // REACT_APP_DIR_NAME=mydir
-    // REACT_APP_REGION=us-east-1
-    // const config = {
-    //     bucketName: "invtest2",
-    //     region: "us-east-1",
-    //     accessKeyId: "AKIAXJTZRA37HBWZUM4U",
-    //     secretAccessKey: "RdMu2JiT0mHvJTKzKg7Zmp4rP9RTi2r2RfTpjnWnXXXXXX"
-    // };
 
 
     return (
@@ -58,14 +31,6 @@ function App() {
                     onRecordingComplete={videoBlob => {
                         // Do something with the video...
                         console.log('videoBlob', videoBlob)
-
-                        // const ReactS3Client = new S3(config);
-                        // ReactS3Client.uploadFile(videoBlob, 'video.mp4').then(
-                        //     data =>{
-                        //         console.log(data);
-                        //     }
-                        //
-                        // )
 
                         s3.putObject({
                                 Key: "video.mp4",
