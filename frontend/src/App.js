b99b07d80c3f6c501ec1e47ed1291cc88788d66a
Shan Wijenayaka <shanwijenayaka@outlook.com>
2021-03-26 22:38:47 +0530

    css added set path with current timestamp and a uuid

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import VideoRecorder from 'react-video-recorder';
 import * as AWS from "aws-sdk";
+import { v4 as uuidv4 } from 'uuid';
 
 
 function App() {
@@ -12,6 +13,11 @@ function App() {
         IdentityPoolId: 'us-east-1:f1899ed5-00c6-4808-943a-ba0ef645a8f7',
     });
 
+    const userId = new Date().valueOf() + "_" + uuidv4();
+
+    const videoFileName = `${userId}.mp4`;
+    const s3BasePath = `https://shanwije-inv-0419.s3.amazonaws.com/${videoFileName}`;
+
     const s3 = new AWS.S3({
         params: {
             Bucket: 'shanwije-inv-0419'
@@ -21,14 +27,17 @@ function App() {
 
     return (
         <div className="App">
-            <header className="App-header">
                 <VideoRecorder
+
+
+
+
                     onRecordingComplete={videoBlob => {
                         // Do something with the video...
                         console.log('videoBlob', videoBlob)
 
                         s3.putObject({
-                                Key: "video.mp4",
+                                Key: videoFileName,
                                 Body: videoBlob,
                                 'ContentType': 'video/mp4',
                                 ACL: 'public-read'
@@ -38,13 +47,13 @@ function App() {
                                     console.log(err)
                                 } else {
                                     console.log("success")
+                                    console.log(s3BasePath)
                                     // On Success
                                 }
                             }
                         )
                     }}
                 />
-            </header>
         </div>
     );
 }
