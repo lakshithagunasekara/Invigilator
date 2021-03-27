8809d49821a557c2997faba89dd284e169de1fc4
Shan Wijenayaka <shanwijenayaka@outlook.com>
2021-03-27 22:18:02 +0530

    set resource to s3 path

--- invigilator-result-service/routes/results.js
+++ invigilator-result-service/routes/results.js
@@ -1,18 +1,33 @@
+const request = require('request');
 
-const frameRoutes = (app, fs) => {
-
-    // variables
-    const s3_bucket_file_path = './data/results.json';
+const frameRoutes = (app) => {
 
     // READ
     app.get('/results', (req, res) => {
-        fs.readFile(s3_bucket_file_path, 'utf8', (err, data) => {
-            if (err) {
-                throw err;
-            }
+        const student_id = req.query.id;
+        if(student_id) {
+            const s3_bucket_file_path = `https://invigilator-s3bucket-qljvzcoqk2zw.s3.amazonaws.com/output/${student_id}/results.json`;
+            request(s3_bucket_file_path, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    try{
+                        res.status(200);
+                        res.send(JSON.parse(body));
+                    } catch (e) {
+                        res.status(500);
+                        console.log("json parse error ", e);
+                        res.send(e);
+                    }
+                } else {
+                    console.log("request error " , response.body)
+                    res.status(400);
+                    res.send(response.body);
+                }
+            });
+        } else {
+            res.status(422);
+            res.send("empty student id");
+        }
 
-            res.send(JSON.parse(data));
-        });
     });
 };
 
